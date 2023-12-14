521f60adeed5678ede7e3ecaca7a24c046683126
hushahadat <shahadathussain757@gmail.com>
2023-12-15 00:00:29 +0530

    .

--- src/component/Table/DashboardTable.jsx
+++ src/component/Table/DashboardTable.jsx
@@ -66,29 +66,46 @@ export const DashboardTable = (props) => {
           name : item.name,
           symbol : item.symbol,
           price : item.quote.USD.price.toFixed(3),
-          market_cap : item.quote.USD.price.toFixed(3)
+          market_cap : item.quote.USD.price.toFixed(3),
+          total_supply : item.total_supply.toFixed(3),
+          percent_change_1h : item.quote.USD.percent_change_1h.toFixed(3),
+          percent_change_30d : item.quote.USD.percent_change_30d.toFixed(3)
         }
       })
-      // console.log("++++++++++++_+_+__+_+_+_=",data)
       settableData(data)
     }
-
   },[coins])
+
+  const onClickRow=(props)=>{
+    console.log("2111111111212",props);
+  }
   return (
   <>
-  <Col className='table-bod' span={24}>
-    <Table bodyStyle={{color : "red"}}  dataSource={tableData}  pagination={{
+  <Col  span={24}>
+    <Table  dataSource={tableData}
+            onRow={(record, rowIndex) => {
+              return {
+                onClick: (e)=>{onClickRow(record)}, // click row
+              };
+            }}
+            rowClassName=''
+          pagination={{
           position: ['bottomCenter'],
           defaultPageSize : ['5'],
-          pageSizeOptions :['3','5','10']
+          pageSizeOptions :['3','5','10'],
+          
         }}>
 
    
-      <Column title="SYMBOL"  width={150}  type={'danger'} columns ={{}} dataIndex="symbol" key="symbol" />
-      <Column title="NAME" dataIndex="name" key="name" />
+      <Column align='center' title="SYMBOL"  dataIndex="symbol" key="symbol" />
+      <Column align='center' title="NAME" dataIndex="name" key="name" />
     
-    <Column title="PRICE" dataIndex="price" key="price" />
-    <Column title="MARKET CAP" dataIndex="market_cap" key="market_cap" />
+    <Column align='center' title="PRICE" dataIndex="price" key="price" />
+    <Column align='center' title="MARKET CAP" dataIndex="market_cap" key="market_cap" />
+    
+    <Column align='center' title="PERCENTAGE CHANGE 1H" dataIndex="percent_change_1h" key="percent_change_1h" />
+    <Column align='center' title="PERCENTAGE CHANGE 30D" dataIndex="percent_change_30d" key="percent_change_30d" />
+    <Column align='center' title="TOTAL SUPPLY" dataIndex="total_supply" key="total_supply" />
     
   </Table>
   </Col>
